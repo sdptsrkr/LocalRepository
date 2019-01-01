999b9c886dc40d5d99faba85b648a10fd0e9f12a
Sudipto <sudiptosarkar@outlook.com>
2019-01-01 23:56:21 +0530

    Test

---
+++ in.co.test.springboot/src/main/java/springboot/in/co/test/springboot/App.java
@@ -0,0 +1,15 @@
+package springboot.in.co.test.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Testing GIT checkin!" );
+        
+    }
+}
